d81c84c4386dad15a521539c42f5808b749d1b0f
akon1te <timmiakov@gmail.com>
2023-06-07 18:23:28 +0300

    update paths and gitignore

--- new_features/scripts/sound_exctraction.py
+++ new_features/scripts/sound_exctraction.py
@@ -1,15 +1,14 @@
 from moviepy.editor import *
 from pathlib import Path
-from pickle_loader import load_pickle
-
-DATA_PATH = Path('../sdk_features/')
+from tools import load_pickle
+from tqdm import tqdm
 
 
 def exctraction():
-    target_dict = load_pickle(DATA_PATH / "humor_label_sdk.pkl")
-    for idx in target_dict.keys():
-        video = VideoFileClip(f'../data/urfunny2_video/{idx}.mp4')
-        video.audio.write_audiofile(f'../data/urfunny2_audio/{idx}.mp3')
+    target_dict = load_pickle("../../data/humor_label_sdk.pkl")
+    for idx in tqdm(target_dict.keys()):
+        video = VideoFileClip(f'../../data/urfunny2_video/{idx}.mp4')
+        video.audio.write_audiofile(f'../../data/urfunny2_audio/{idx}.mp3')
 
 
 if __name__ == "__main__":
